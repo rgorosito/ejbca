b62b6256e3ed3a4c0647b8d4b609c92a70f4978f
jeklund <jeklund@37aeb1de-b14b-0410-9a0e-82b28170d228>
2010-01-08 13:12:51 +0000

    ECA-1465: Use a common log4j configuration file for External RA CLI and use the right JAR from bash-script.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca/trunk/ejbca@8532 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/externalra/resources/externalra-cli.sh
+++ modules/externalra/resources/externalra-cli.sh
@@ -5,4 +5,4 @@ if [ -z ${EXTERNALRACLI_HOME} ] ; then
 	EXTERNALRACLI_HOME=`echo $(dirname ${0})`
 fi
 
-java -Djava.endorsed.dirs=${EXTERNALRACLI_HOME}/endorsed -jar $EXTERNALRACLI_HOME/clientToolBox.jar "${@}"
+java -Djava.endorsed.dirs=${EXTERNALRACLI_HOME}/endorsed -jar $EXTERNALRACLI_HOME/externalra-cli.jar "${@}"
