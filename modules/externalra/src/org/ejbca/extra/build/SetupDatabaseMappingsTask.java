8178ba34d2e95480c7cf1cdb591144a674915d26
malu9369 <malu9369@37aeb1de-b14b-0410-9a0e-82b28170d228>
2012-02-09 12:04:55 +0000

    ECA-2330: Add missing version header and javadoc for the custom Ant task class.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca/trunk/ejbca@14029 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/externalra/src/org/ejbca/extra/build/SetupDatabaseMappingsTask.java
+++ modules/externalra/src/org/ejbca/extra/build/SetupDatabaseMappingsTask.java
@@ -1,9 +1,29 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.ejbca.extra.build;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 
+/**
+ * A custom Ant task to run the setup-database-mappings-fil Ant target
+ * for each instance of the externalra.source properties in externalra.properties
+ * 
+ * @version $Id$
+ *
+ */
 public class SetupDatabaseMappingsTask extends Task {
 
     @Override
