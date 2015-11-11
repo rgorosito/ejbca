893ddc32467bdee90eb692f6754e1d41c8db9487
aveen4711 <aveen4711@37aeb1de-b14b-0410-9a0e-82b28170d228>
2015-11-11 12:23:43 +0000

    ECA-4600: Changed parent class
    git-svn-id: https://svn.cesecore.eu/svn/ejbca/trunk/ejbca@22167 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/cmpclient/src/org/ejbca/ui/cmpclient/commands/CmpCommandBase.java
+++ modules/cmpclient/src/org/ejbca/ui/cmpclient/commands/CmpCommandBase.java
@@ -11,11 +11,11 @@ package org.ejbca.ui.cmpclient.commands;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.cmp.PKIMessage;
+import org.ejbca.ui.cli.infrastructure.command.CommandBase;
 import org.ejbca.ui.cli.infrastructure.command.CommandResult;
-import org.ejbca.ui.cli.infrastructure.command.PasswordUsingCommandBase;
 import org.ejbca.ui.cli.infrastructure.parameter.ParameterContainer;
 
-public abstract class CmpCommandBase extends PasswordUsingCommandBase {
+public abstract class CmpCommandBase extends CommandBase {
 
     public abstract PKIMessage generatePKIMessage(ParameterContainer parameters) throws Exception;
     
