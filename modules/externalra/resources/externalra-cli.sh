3710ae43ee9ee6e3bc32b40003ac430789b50413
Ricardo A. Gorosito <rgorosito@gmail.com>
2016-08-20 12:54:49 -0300

    resync with upstream source

--- modules/externalra/resources/externalra-cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-if [ -z ${EXTERNALRACLI_HOME} ] ; then
-	EXTERNALRACLI_HOME=`echo $(dirname ${0})`
-fi
-
-java -Djava.endorsed.dirs=${EXTERNALRACLI_HOME}/endorsed -jar $EXTERNALRACLI_HOME/externalra-cli.jar "${@}"
