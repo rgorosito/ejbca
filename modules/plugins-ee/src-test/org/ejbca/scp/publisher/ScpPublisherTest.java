c7c639339bae1cbc90d6cda8a4df2c6ea6287d2a
mikekushner <mikekushner@37aeb1de-b14b-0410-9a0e-82b28170d228>
2018-11-01 13:21:29 +0000

    ECA-7425: Initial checkin of SCP Publisher
    git-svn-id: https://svn.cesecore.eu/svn/ejbca/trunk/ejbca@30348 37aeb1de-b14b-0410-9a0e-82b28170d228

---
+++ modules/plugins-ee/src-test/org/ejbca/scp/publisher/ScpPublisherTest.java
@@ -0,0 +1,70 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA - Proprietary Modules: Enterprise Certificate Authority        *
+ *                                                                       *
+ *  Copyright (c), PrimeKey Solutions AB. All rights reserved.           *
+ *  The use of the Proprietary Modules are subject to specific           * 
+ *  commercial license terms.                                            *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.scp.publisher;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
+
+import org.bouncycastle.operator.OperatorCreationException;
+import org.cesecore.certificates.certificate.CertificateConstants;
+import org.cesecore.certificates.util.AlgorithmConstants;
+import org.cesecore.keys.util.KeyTools;
+import org.cesecore.util.CertTools;
+import org.cesecore.util.CryptoProviderTools;
+import org.cesecore.util.TraceLogMethodsRule;
+import org.ejbca.core.model.ca.publisher.PublisherException;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+/**
+ * 
+ * This test provides some simple boilerplate to test an scp to a known server. Tests are set to ignore until somebody figures out how to makes this test 
+ * work universally. 
+ * 
+ * @version $Id$
+ *
+ */
+public class ScpPublisherTest {
+
+    @Rule
+    public TestRule traceLogMethodsRule = new TraceLogMethodsRule();
+    
+    @BeforeClass
+    public static void beforeClass() {
+        CryptoProviderTools.installBCProviderIfNotAvailable();
+    }
+
+    @Ignore
+    @Test
+    public void testScpFunctionality() throws PublisherException, OperatorCreationException, CertificateException, InvalidAlgorithmParameterException {
+        ScpPublisher scpPublisher = new ScpPublisher();
+        Properties properties = new Properties();
+        properties.setProperty(ScpPublisher.ANONYMIZE_CERTIFICATES_PROPERTY_NAME, "false");
+        properties.setProperty(ScpPublisher.CERT_SCP_DESTINATION_PROPERTY_NAME, "download.primekey.com:tmp");
+        properties.setProperty(ScpPublisher.CRL_SCP_DESTINATION_PROPERTY_NAME, "download.primekey.com:tmp");
+        properties.setProperty(ScpPublisher.SCP_PRIVATE_KEY_PROPERTY_NAME, "/Users/mikek/.ssh/id_rsa");
+        properties.setProperty(ScpPublisher.SCP_KNOWN_HOSTS_PROPERTY_NAME, "/Users/mikek/.ssh/known_hosts");
+        properties.setProperty(ScpPublisher.SSH_USERNAME, "mikek");
+        properties.setProperty(ScpPublisher.SCP_PRIVATE_KEY_PASSWORD, "");
+        scpPublisher.init(properties);
+        KeyPair keys = KeyTools.genKeys("512", AlgorithmConstants.KEYALGORITHM_RSA); 
+        X509Certificate certificate = CertTools.genSelfCert("C=SE,O=PrimeKey,CN=ScpPublisherTest", 365, null, keys.getPrivate(), keys.getPublic(),
+                AlgorithmConstants.SIGALG_SHA1_WITH_RSA, true);        
+        scpPublisher.storeCertificate(null, certificate, null, null, null, null, CertificateConstants.CERT_REVOKED, 
+                CertificateConstants.CERTTYPE_ENDENTITY, 0, 0, null, 0, 0, null);
+    }
+
+}
