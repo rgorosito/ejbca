81cfdc747a93e7b2b1c69cdbc9efbeb80266d219
aminkh <aminkh@37aeb1de-b14b-0410-9a0e-82b28170d228>
2019-10-22 11:24:32 +0000

    ECA-8635: Add util classes.
    git-svn-id: https://svn.cesecore.eu/svn/ejbca/trunk/ejbca@33676 37aeb1de-b14b-0410-9a0e-82b28170d228

--- modules/systemtests/src-test/org/cesecore/certificates/certificatetransparency/CertificateTransparencyTest.java
+++ modules/systemtests/src-test/org/cesecore/certificates/certificatetransparency/CertificateTransparencyTest.java
@@ -9,7 +9,10 @@
  *************************************************************************/
 package org.cesecore.certificates.certificatetransparency;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.UnknownHostException;
 import java.security.KeyPair;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
@@ -21,22 +24,36 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.x509.ReasonFlags;
+import org.cesecore.CaTestUtils;
 import org.cesecore.authentication.tokens.AuthenticationToken;
+import org.cesecore.certificates.ca.CAInfo;
 import org.cesecore.certificates.certificate.CertificateStoreSessionRemote;
 import org.cesecore.certificates.certificate.CertificateWrapper;
+import org.cesecore.certificates.certificate.InternalCertificateStoreSessionRemote;
 import org.cesecore.certificates.certificateprofile.CertificateProfile;
+import org.cesecore.certificates.certificateprofile.CertificateProfileConstants;
+import org.cesecore.certificates.endentity.EndEntityConstants;
+import org.cesecore.certificates.endentity.EndEntityInformation;
+import org.cesecore.certificates.endentity.EndEntityTypes;
 import org.cesecore.certificates.util.AlgorithmConstants;
 import org.cesecore.keys.util.KeyTools;
 import org.cesecore.keys.util.PublicKeyWrapper;
 import org.cesecore.mock.authentication.tokens.TestAlwaysAllowLocalAuthenticationToken;
 import org.cesecore.util.CertTools;
+import org.cesecore.util.CryptoProviderTools;
 import org.cesecore.util.EjbRemoteHelper;
 import org.ejbca.core.ejb.ca.sign.SignSessionRemote;
+import org.ejbca.core.ejb.ra.EndEntityManagementSessionRemote;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import static org.cesecore.certificates.certificatetransparency.CtTestData.LABELS_A;
+import static org.cesecore.certificates.certificatetransparency.CtTestData.LOG_LABEL_A;
 
 /**
  * Test class to run certificate transparency tests
@@ -45,62 +62,95 @@ import static org.junit.Assert.fail;
  *
  */
 public class CertificateTransparencyTest {
-    
-    private static final SignSessionRemote signSession = EjbRemoteHelper.INSTANCE.getRemoteSession(SignSessionRemote.class);
-    private static final CertificateStoreSessionRemote certificateStoreSession = EjbRemoteHelper.INSTANCE.getRemoteSession(CertificateStoreSessionRemote.class);
+
+    private final SignSessionRemote signSession = EjbRemoteHelper.INSTANCE.getRemoteSession(SignSessionRemote.class);
+    private final CertificateStoreSessionRemote certificateStoreSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(CertificateStoreSessionRemote.class);
+    private final EndEntityManagementSessionRemote endEntityManagementSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(EndEntityManagementSessionRemote.class);
+    private final InternalCertificateStoreSessionRemote internalCertStoreSession = EjbRemoteHelper.INSTANCE
+            .getRemoteSession(InternalCertificateStoreSessionRemote.class, EjbRemoteHelper.MODULE_TEST);
 
     private final static Logger log = Logger.getLogger(CertificateTransparencyTest.class);
 
     private static SctDataCallback sctDataCallback;
-    
+
     private static final int LOGSERVER_START_PORT = 8760;
-    
-    private Map<Integer,CTLogInfo> ctlogs;
+
+    private Map<Integer, CTLogInfo> ctlogs;
+    private List<CTLogTestServer> testServers;
     private ServerSocket deadServerSocket;
 
     private final CertificateTransparency ct = new CertificateTransparencyImpl();
 
     private static final String TEST_USER_NAME = "testUserName";
-    
+
     final AuthenticationToken admin = new TestAlwaysAllowLocalAuthenticationToken(TEST_USER_NAME);
-    
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        CryptoProviderTools.installBCProviderIfNotAvailable();
+    }
+
     @Test
     public void testPreCertStoredIfCannotConnectToLogServer() throws Exception {
         log.trace(">testPreCertStoredIfCannotConnectToLogServer");
-        
+
         final KeyPair keyPair = KeyTools.genKeys("2048", AlgorithmConstants.KEYALGORITHM_RSA);
-        //CTLogTest.createTestCTLogServer(LOGSERVER_START_PORT, CtTestData.CTLOG_PUBKEY, CtTestData.REQUEST, CtTestData.RESPONSE1);
-        
-        //CTLogTest.createDeadServerWithLabel(LOGSERVER_START_PORT + 3, CtTestData.CTLOG_PUBKEY, CtTestData.LOG_LABEL_A);
-        
-        final X509Certificate clientCertificate = (X509Certificate) signSession.createCertificate(admin, TEST_USER_NAME, "foo123", new PublicKeyWrapper(keyPair.getPublic()));
-        final List<Certificate> certificateChain = new ArrayList<>();
-        certificateChain.add(clientCertificate);
-        
+
+        final CAInfo caInfo = CaTestUtils.getClientCertCaInfo(admin);
+        String usercertFp="";
+
         try {
-            fetchSCTList(makeTestChain(), 1, 1, CtTestData.LABELS_A, 0);
-            fail("Should throw");
-        } catch (CTLogException e) {
-            // Make sure we fail for the right reason
-            assertTrue("Wrong error message. Was: " + e.getMessage(), e.getMessage().contains("Insufficient SCTs, minimum is 1, but got 0."));
+            final EndEntityInformation user = new EndEntityInformation(TEST_USER_NAME, "CN=" + TEST_USER_NAME + ",O=WebTestUtils", caInfo.getCAId(),
+                    null, null, EndEntityTypes.ENDUSER.toEndEntityType(), 1, CertificateProfileConstants.CERTPROFILE_FIXED_ENDUSER,
+                    EndEntityConstants.TOKEN_USERGEN, null);
+            user.setPassword("foo123");
+            endEntityManagementSession.addUser(admin, user, false);
+
+            final X509Certificate clientCertificate = (X509Certificate) signSession.createCertificate(admin, TEST_USER_NAME, "foo123",
+                    new PublicKeyWrapper(keyPair.getPublic()));
+            assertNotNull("Returned client certificate was null", clientCertificate);
+            usercertFp = CertTools.getFingerprintAsString(clientCertificate);
+
+
+            createTestCTLogServer(LOGSERVER_START_PORT, CtTestData.CTLOG_PUBKEY, CtTestData.REQUEST, CtTestData.RESPONSE1);
+
+            createDeadServerWithLabel(LOGSERVER_START_PORT + 3, CtTestData.CTLOG_PUBKEY, CtTestData.LOG_LABEL_A);
+
+            final List<Certificate> certificateChain = new ArrayList<>();
+            certificateChain.add(clientCertificate);
+
+            try {
+                fetchSCTList(makeTestChain(), 1, 1, LABELS_A, 0);
+                fail("Should throw");
+            } catch (CTLogException e) {
+                // Make sure we fail for the right reason
+                assertTrue("Wrong error message. Was: " + e.getMessage(), e.getMessage().contains("Insufficient SCTs, minimum is 1, but got 0."));
+            }
+
+            final String fingerprint = CertTools.getFingerprintAsString(clientCertificate);
+            CertificateWrapper certWrapper = certificateStoreSession.findCertificateByFingerprintRemote(fingerprint);
+
+            assertNotNull("Certificate for the data was null!", certWrapper.getCertificate());
+            assertNotNull("Poison field for the certificate was null!",
+                    ((X509Extension) certWrapper.getCertificate()).getExtensionValue("1.3.6.1.4.1.11129.2.4.3"));
+
+        } finally {
+            // Remove it to clean database
+            internalCertStoreSession.removeCertificate(usercertFp);
+            endEntityManagementSession.revokeAndDeleteUser(admin, TEST_USER_NAME, ReasonFlags.unused);
         }
-        
-        final String fingerprint = CertTools.getFingerprintAsString(clientCertificate);
-        CertificateWrapper certWrapper = certificateStoreSession.findCertificateByFingerprintRemote(fingerprint);
- 
-        assertNotNull("Certificate for the data was null!", certWrapper.getCertificate());
-        assertNotNull("Poison field for the certificate was null!", ((X509Extension) certWrapper.getCertificate()).getExtensionValue("1.3.6.1.4.1.11129.2.4.3"));
-        
         log.trace("<testPreCertStoredIfCannotConnectToLogServer");
     }
-    
+
     static List<Certificate> makeTestChain() {
         final List<Certificate> chain = new ArrayList<>();
         chain.add(pemToCert(CtTestData.TESTCERT_PRECERTIFICATE));
         chain.add(pemToCert(CtTestData.ISSUER_CERT));
         return chain;
     }
-    
+
     static Certificate pemToCert(final String pem) {
         try {
             final byte[] bytes = KeyTools.getBytesFromPEM(pem, CertTools.BEGIN_CERTIFICATE, CertTools.END_CERTIFICATE);
@@ -109,13 +159,13 @@ public class CertificateTransparencyTest {
             throw new IllegalArgumentException(e);
         }
     }
-    
+
     private byte[] fetchSCTList(final List<Certificate> chain, final int minSCTs, final int maxSCTs, final LinkedHashSet<String> labels,
             final int maxRetries) throws CTLogException {
         return fetchSCTList(ct, ctlogs, chain, minSCTs, maxSCTs, labels, maxRetries);
     }
-    
-    private static byte[] fetchSCTList(final CertificateTransparency ct, final Map<Integer,CTLogInfo> ctlogs, final List<Certificate> chain,
+
+    private static byte[] fetchSCTList(final CertificateTransparency ct, final Map<Integer, CTLogInfo> ctlogs, final List<Certificate> chain,
             final int minSCTs, final int maxSCTs, final LinkedHashSet<String> labels, final int maxRetries) throws CTLogException {
         final CertificateProfile certProfile = new CertificateProfile();
         certProfile.setUseCertificateTransparencyInCerts(true);
@@ -130,7 +180,7 @@ public class CertificateTransparencyTest {
         config.setValidityPolicy(new GoogleCtPolicy());
         return ct.fetchSCTList(chain, certProfile, config, sctDataCallback);
     }
-    
+
     private void addLog(final int port, final boolean tls, final String logKeyPEM, final String label) {
         final byte[] pubKeyBytes = KeyTools.getBytesFromPEM(logKeyPEM, CertTools.BEGIN_PUBLIC_KEY, CertTools.END_PUBLIC_KEY);
         final String protocol = (tls ? "https" : "http");
@@ -139,4 +189,24 @@ public class CertificateTransparencyTest {
         ctlogs.put(ctlog.hashCode(), ctlog);
     }
     
+    public void createTestCTLogServer(final int port, final String logKeyPEM, final String requestContent, final String responseContent) throws UnknownHostException, IOException {
+        addLog(port, false, logKeyPEM, LOG_LABEL_A);
+        testServers.add(new CTLogTestServer("POST", "/ct/v1/add-pre-chain",
+            "application/json", requestContent, "application/json", responseContent, port, false, 0L));
+    }
+    
+    private void createCTLogServerWithLabel(final int port, final String logKeyPEM, final String requestContent, final String responseContent, final String label)
+            throws UnknownHostException, IOException {
+        addLog(port, false, logKeyPEM, label);
+        testServers.add(new CTLogTestServer("POST", "/ct/v1/add-pre-chain", "application/json", requestContent, "application/json", responseContent,
+                port, false, 0L));
+    }
+    
+    public void createDeadServerWithLabel(final int port, final String logKeyPEM, final String label) throws IOException {
+        addLog(port, false, logKeyPEM, label);
+        deadServerSocket = new ServerSocket();
+        deadServerSocket.setReuseAddress(true);
+        deadServerSocket.bind(new InetSocketAddress("127.0.0.1", port));
+    }
+
 }
